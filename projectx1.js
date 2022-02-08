fdd3f4f3eee620e427d8b4621735bfb2a713b4c2
Urdo <karol.ugarenko@gmail.com>
2022-02-08 20:25:24 +0100

    fix password show

--- projectx1.js
+++ projectx1.js
@@ -7,6 +7,7 @@ input.addEventListener('input', (e)=> {
     console.log(e.target.value)
     if (password === e.target.value) {
         document.querySelector('div').textContent= message;
+        e.target.value =""; // invisible password when correct
     } else {
         document.querySelector('div').textContent=messageBad;
     }
