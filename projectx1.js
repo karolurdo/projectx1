cf7a3b59c5463d61c7c3d6272f9139cbf67b73f6
Urdo <karol.ugarenko@gmail.com>
2022-02-08 20:22:35 +0100

    check password

--- projectx1.js
+++ projectx1.js
@@ -1,5 +1,13 @@
 const input = document.getElementById('pass'); //getElementById from HTML
+const message = "password accept";
+const messageBad = "wrong password";
+const password = "admin";
 
 input.addEventListener('input', (e)=> {
     console.log(e.target.value)
+    if (password === e.target.value) {
+        document.querySelector('div').textContent= message;
+    } else {
+        document.querySelector('div').textContent=messageBad;
+    }
 })
