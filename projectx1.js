01b877317194f3b1e7bec97400577efec92235a4
Urdo <karol.ugarenko@gmail.com>
2022-02-09 21:01:21 +0100

    check more passowrds

--- projectx1.js
+++ projectx1.js
@@ -4,21 +4,26 @@ const messageBad = "wrong password";
 const passwords = ["admin", "user", "noob"]; // password
 
 input.addEventListener('input', (e) => {
-    console.log(e.target.value)
-    if (password === e.target.value) {
+    
+    const text = e.target.value;
+    document.querySelector('div').textContent=messageBad;
+    
+passwords.forEach(password=> {
+    if (password === text) {
         document.querySelector('div').textContent= message;
-        e.target.value =""; // invisible password when correct
-    } else {
-        document.querySelector('div').textContent=messageBad;
-    }
-})
+        e.target.value='';
+}
+
+});
+});
+
 
 //FOCUS
-input.addEventListener('focus', (e) => { //change backgroud color when write password
-    e.target.classList.add('active');
-})
+ input.addEventListener('focus', (e) => { //change backgroud color when write password
+ e.target.classList.add('active');
+ });
 
 //BLUR
 input.addEventListener('blur', (e) => { 
-    e.target.classList.remove('active');
-})
+e.target.classList.remove('active');
+});
